22a079e9b2ddeb1d1cca1decb9c5268b54fa380f
wenjing <598228662@qq.com>
2016-07-20 17:08:32 +0800

    update

--- Code/generator.js
+++ Code/generator.js
@@ -8,3 +8,16 @@ var hw = helloWorldGenerator();
 hw.next();
 hw.next();
 hw.next();
+
+function* foo() {
+	yield 1;
+	yield 2;
+	yield 3;
+	yield 4;
+	yield 5;
+	return 6;
+}
+
+for( let v of foo()) {
+	console.log(v);
+}
